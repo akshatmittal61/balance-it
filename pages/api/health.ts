c9d9f54657ee79c6ff36d30aee93a7b6170c0e7e
Akshat Mittal <akshatmittal2506@gmail.com>
2025-05-11 01:58:59 +0530

    feat: added health check

---
+++ pages/api/health.ts
@@ -0,0 +1,21 @@
+import { DatabaseManager } from "@/connections";
+import { apiMethods, dbUri, HTTP } from "@/constants";
+import { ServerController } from "@/controllers";
+import { ApiRequest, ApiResponse } from "@/types";
+import { ApiFailure } from "@/utils";
+
+const handler = async (req: ApiRequest, res: ApiResponse) => {
+	const dbContainer = DatabaseManager.createContainer(dbUri);
+	await dbContainer.db.connect().catch(() => {});
+	if (req.method === apiMethods.GET) {
+		return ServerController.health(dbContainer.db)(req, res);
+	} else {
+		res.setHeader("Allow", apiMethods.GET);
+		return new ApiFailure(res)
+			.status(HTTP.status.METHOD_NOT_ALLOWED)
+			.message(`Method ${req.method} Not Allowed`)
+			.send();
+	}
+};
+
+export default handler;
