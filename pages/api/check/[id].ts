13805abca415c9c516b55c4556c5e86fe7f6871b
Akshat Mittal <akshatmittal2506@gmail.com>
2025-05-25 17:26:58 +0530

    test: new api route

--- pages/api/check/[id].ts
+++ pages/api/check/[id].ts
@@ -2,11 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === "GET") {
-		return res.status(200).json(req.query);
+		res.status(200).json(req.query);
 	} else {
-		return res
-			.status(200)
-			.json({ message: "Wan'nt GET", query: req.query });
+		res.status(200).json({ message: "Wasn't GET", query: req.query });
 	}
 };
 
--- pages/api/v1/wallet/expenses/[id]/index.ts
+++ pages/api/v1/wallet/expenses/[id]/index.ts
@@ -1,4 +1,4 @@
-/* import { WalletController } from "@/controllers";
+import { WalletController } from "@/controllers";
 import { ApiRoute } from "@/server";
 
 const apiRoute = new ApiRoute(
@@ -10,18 +10,3 @@ const apiRoute = new ApiRoute(
 );
 
 export default apiRoute.getHandler();
- */
-
-import { NextApiRequest, NextApiResponse } from "next";
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-	if (req.method === "GET") {
-		return res.status(200).json(req.query);
-	} else {
-		return res
-			.status(200)
-			.json({ message: "Wan'nt GET", query: req.query });
-	}
-};
-
-export default handler;
