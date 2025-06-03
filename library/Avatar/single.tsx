c6b92ed84d776cb128ec89a8351a93ea0097b3a9
Akshat Mittal <akshatmittal2506@gmail.com>
2025-06-03 09:54:31 +0530

    fix: avatar infinite requests fix

--- library/Avatar/single.tsx
+++ library/Avatar/single.tsx
@@ -1,9 +1,9 @@
 import { fallbackAssets } from "@/constants";
-import { getImageUrlFromDriveLink, stylesConfig } from "@/utils";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import { stylesConfig } from "@/utils";
+import React from "react";
 import styles from "./styles.module.scss";
 import { IAvatarProps } from "./types";
+import { AvatarUtils } from "./utils";
 
 const classes = stylesConfig(styles);
 
@@ -18,38 +18,7 @@ export const Avatar: React.FC<IAvatarProps> = ({
 	isClickable,
 	...props
 }) => {
-	const [isImageValid, setIsImageValid] = useState(
-		src && (src.startsWith("https://") || src.startsWith("/"))
-			? true
-			: false
-	);
-	const imageUrl = (() => {
-		if (src && (src.startsWith("https://") || src.startsWith("/"))) {
-			return getImageUrlFromDriveLink(src);
-		}
-		return "";
-	})();
-
-	const getAvatarSize = () => {
-		switch (size) {
-			case "small":
-				return 100;
-			case "medium":
-				return 150;
-			case "large":
-				return 200;
-			default:
-				return typeof size === "number" ? size : 50;
-		}
-	};
-
-	useEffect(() => {
-		setIsImageValid(
-			src && (src.startsWith("https://") || src.startsWith("/"))
-				? true
-				: false
-		);
-	}, [src, fallback]);
+	const imageUrl = AvatarUtils.getImageUrl(src);
 
 	return (
 		<div
@@ -63,35 +32,21 @@ export const Avatar: React.FC<IAvatarProps> = ({
 			title={alt}
 			{...props}
 			style={{
-				width: getAvatarSize(),
-				height: getAvatarSize(),
-				cursor:
-					onClick && typeof onClick === "function"
-						? "pointer"
-						: "auto",
+				width: AvatarUtils.getAvatarSize(size),
+				height: AvatarUtils.getAvatarSize(size),
 				...props.style,
 			}}
 		>
-			{isImageValid ? (
-				<Image
-					src={imageUrl}
-					alt={alt + ""}
-					width={getAvatarSize() * 2}
-					height={getAvatarSize() * 2}
-					className={classes("avatar-image")}
-					onError={() => {
-						setIsImageValid(false);
-					}}
-				/>
-			) : (
-				<Image
-					src={fallback}
-					alt={alt + ""}
-					width={getAvatarSize() * 2}
-					height={getAvatarSize() * 2}
-					className={classes("avatar-image")}
-				/>
-			)}
+			<img
+				src={imageUrl}
+				alt={alt + ""}
+				width={AvatarUtils.getAvatarSize(size) * 2}
+				height={AvatarUtils.getAvatarSize(size) * 2}
+				className={classes("avatar-image")}
+				onError={(e) => {
+					e.currentTarget.src = AvatarUtils.getFallbackAvatarUrl(alt);
+				}}
+			/>
 		</div>
 	);
 };
--- utils/string.ts
+++ utils/string.ts
@@ -1,8 +1,8 @@
 export class StringUtils {
-	public static isEmpty(s: string) {
+	public static isEmpty(s: string | null | undefined) {
 		return s === undefined || s === null || s === "";
 	}
-	public static isNotEmpty(s: string) {
+	public static isNotEmpty(s: string | null | undefined) {
 		return !StringUtils.isEmpty(s);
 	}
 }
